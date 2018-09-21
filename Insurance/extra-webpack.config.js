b41dc174052148612ec8b2621bdd66bcdc14d216
WilliamBZA <william.brander@gmail.com>
2018-09-21 11:55:21 +0200

    Tweak vue demo

--- Insurance/extra-webpack.config.js
+++ Insurance/extra-webpack.config.js
@@ -3,7 +3,6 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
     module: {
         rules: [
-            // ... other rules
             {
               test: /\.vue$/,
               loader: 'vue-loader'
--- Insurance/src/app/parcel.component.ts
+++ Insurance/src/app/parcel.component.ts
@@ -20,12 +20,62 @@ import singleSpaVue from 'single-spa-vue';
         const vueLifecycles = singleSpaVue({
             Vue,
             appOptions: {
-              templateUrl: './vueComponent.html'
-            }
-          });
+              template: '<div id="demo">\n' +
+              '<form id="search">\n' +
+                'Search <input name="query" v-model="searchQuery">\n' +
+              '</form>\n' +
+              '<table>\n' +
+                '<thead>\n' +
+                  '<tr>\n' +
+                    '<th v-for="key in columns"\n' +
+                    '@click="sortBy(key)"\n' +
+                    ':class="{ active: sortKey == key }">\n' +
+                      '{{ key | capitalize }}\n' +
+                    '</th>\n' +
+                  '</tr>\n' +
+                '</thead>\n' +
+                '<tbody>\n' +
+                  '<tr v-for="entry in filteredData">\n' +
+                    '<td v-for="key in columns">\n' +
+                      '{{entry[key]}}\n' +
+                    '</td>\n' +
+                  '</tr>\n' +
+                '</tbody>\n' +
+              '</table>\n' +
+            '</div>',
+            computed: {
+              filteredData: function () {
+                var filterKey = this.searchQuery && this.searchQuery.toLowerCase();
+                var data = this.data;
+                if (filterKey) {
+                  data = data.filter(function (row) {
+                    return Object.keys(row).some(function (key) {
+                      return String(row[key]).toLowerCase().indexOf(filterKey) > -1;
+                    });
+                  })
+                }
+                return data;
+              }
+            },
+            filters: {
+              capitalize: function (str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+              }
+            },
+          },
+        });
 
         const domElement = document.getElementById('parcelContainer');
-        const parcelProps = {domElement, customProp1: 'foo'};
+        const parcelProps = { domElement, customProp1: 'foo',
+          searchQuery: '',
+          columns: ['name', 'power'],
+          data: [
+            { name: 'Chuck Norris', power: Infinity },
+            { name: 'Bruce Lee', power: 9000 },
+            { name: 'Jackie Chan', power: 7000 },
+            { name: 'Jet Li', power: 8000 }
+          ]
+        };
         const parcel = mountRootParcel(vueLifecycles, parcelProps);
 
         // The parcel is being mounted. We can wait for it to finish with the mountPromise.
