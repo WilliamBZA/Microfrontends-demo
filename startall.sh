4a083cc1c004c34f45141e0b8aa4f73be4742910
WilliamBZA <william.brander@gmail.com>
2018-12-20 08:21:00 +0200

    Add start.sh

---
+++ startall.sh
@@ -0,0 +1,29 @@
+cd ..
+
+cd microfrontends-cruises/
+npm run hostng
+start code .
+
+cd ..
+cd microfrontends-demo/
+
+cd checkout/
+cd expressCheckout/
+npm start
+start code .
+
+cd ..
+cd ..
+
+cd HotelBooking/
+npm start
+start code .
+
+cd ..
+
+cd Insurance/
+start ng serve
+start code .
+
+cd ..
+
